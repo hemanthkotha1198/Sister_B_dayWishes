7814ce80b3ec50065b27cd8ded7b488f99c8067c
praneethguptha <123263232+hemanthkotha1198@users.noreply.github.com>
2023-07-27 20:30:54 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,31 @@
+
+var config = {
+
+    texts: [
+        "give it to me",     
+        "beloved little cutie", 
+        "today is your birthday",
+        "this is what we are together",
+        "best birthday",
+        "last year's bithday",
+        "Do you still remember eating skewers at Wang Po? ",
+        "I'm going to eat well this year",
+        "To feed my cute pigs",
+        "then take away",
+        "Likki~~",
+    ],
+    imgs: {
+        "beloved little cutie": "./imgs/likki.jpg",
+    },
+
+    desc: {
+        turn_on: "start",
+        play: "music",
+        bannar_coming: "color",
+        balloons_flying: "It seems that something is missing",
+        cake_fadein: "cake?",
+        light_candle: "Candle? ",
+        wish_message: "Happy birthday",
+        story: "A MESSAGE FOR YOU",
+    }
+};
